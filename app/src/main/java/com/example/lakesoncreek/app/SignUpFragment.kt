dfa3da97bfe3c825ff8f35f6d19fe0d9658471a8
unknown <sonykarl14@gmail.com>
2021-11-03 17:24:13 +0300

    added navigation

--- app/src/main/java/com/example/lakesoncreek/app/SignUpFragment.kt
+++ app/src/main/java/com/example/lakesoncreek/app/SignUpFragment.kt
@@ -6,15 +6,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.lakesoncreek.R
+import com.example.lakesoncreek.databinding.FragmentSignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpFragment : Fragment() {
+
+    lateinit var auth: FirebaseAuth
+    lateinit var binding: FragmentSignUpBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        auth = FirebaseAuth.getInstance()
+        binding.SignUp.setOnClickListener {
+            val email = binding.EmailSignUp.text.toString()
+            val password = binding.passwordsignup.text.toString()
+            registerUser(email, password)
+        }
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        return binding.root
+    }
+
+    private fun registerUser(email:String, password: String){
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+            task ->
+            if (task.isSuccessful){
+                binding.EmailSignUp.text.clear()
+                binding.passwordsignup.text.clear()
+            }
+        }
     }
 
 }
