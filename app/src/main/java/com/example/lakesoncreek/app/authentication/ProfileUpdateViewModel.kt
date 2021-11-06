ee866a388a60692bc6febf07d5c1f7581111fbcd
unknown <sonykarl14@gmail.com>
2021-11-06 11:31:54 +0300

    added updateprofile viewmodel

--- app/src/main/java/com/example/lakesoncreek/app/authentication/ProfileUpdateViewModel.kt
+++ app/src/main/java/com/example/lakesoncreek/app/authentication/ProfileUpdateViewModel.kt
@@ -1,12 +1,20 @@
 package com.example.lakesoncreek.app.authentication
 
+import android.net.Uri
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.lakesoncreek.data.repository.LakesonRepositoryImp
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ProfileUpdateViewModel @Inject
-constructor(val repositoryImp: LakesonRepositoryImp){
+constructor(val repositoryImp: LakesonRepositoryImp): ViewModel(){
 
-    fun updateProfile(){}
+    fun updateProfile(fullname:String, photo: Uri?, navigation: Unit){
+        viewModelScope.launch {
+            repositoryImp.updateProfile(fullname, photo, navigation)
+        }
+    }
 }
