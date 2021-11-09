1fa6debc72fd6ff0ff6f6b2dca26a3a8675d6725
unknown <sonykarl14@gmail.com>
2021-11-09 19:20:33 +0300

    fixes to user

--- app/src/main/java/com/example/lakesoncreek/data/models/User.kt
+++ app/src/main/java/com/example/lakesoncreek/data/models/User.kt
@@ -4,7 +4,7 @@ data class User(
     val id: String = "",
     val firstName: String = "",
     val lastName: String = "",
-    val email: String = ""
+    val email: String = "",
     val image: String = "",
     val mobile: Long = 0,
     val gender: String = "person",
