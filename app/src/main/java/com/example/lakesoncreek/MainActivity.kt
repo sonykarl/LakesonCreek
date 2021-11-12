f8281801f3e2ffc7050e215e9528115416581744
unknown <sonykarl14@gmail.com>
2021-11-12 15:25:51 +0300

    fixed main activity

--- app/src/main/java/com/example/lakesoncreek/MainActivity.kt
+++ app/src/main/java/com/example/lakesoncreek/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.lakesoncreek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lakesoncreek.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
